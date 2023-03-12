ee6b110cb8ff2438670b120117207b911b35a656
JustKilli <justkilli@outlook.de>
2023-03-12 22:01:04 +0100

    Changed how MainWindow designs its components

--- src/main/java/gui/windows/MainWindow.java
+++ src/main/java/gui/windows/MainWindow.java
@@ -27,6 +27,7 @@ public class MainWindow extends JFrame {
      * */
     private void init() {
         setSize(500, 350);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
@@ -35,11 +36,10 @@ public class MainWindow extends JFrame {
      * */
     private void build() {
         pnlMain = new JPanel(new GridLayout(4, 1, 10, 20));
-        pnlMain.setBackground(design.getBackgroundColor());
-        pnlMain.setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 40));
 
         buildPasswordView();
         buildPasswordOptions();
+        design(design);
 
         add(pnlMain);
     }
@@ -48,23 +48,14 @@ public class MainWindow extends JFrame {
      * */
     private void buildPasswordView() {
         lblPaswordViewDesc = new JLabel("Password");
-        lblPaswordViewDesc.setFont(design.getTextFont());
-        lblPaswordViewDesc.setForeground(design.getTextColor());
-        lblPaswordViewDesc.setBackground(design.getBackgroundComponents());
         lblPaswordViewDesc.setVerticalAlignment(SwingConstants.BOTTOM);
 
         tfPasswordView = new JTextField();
-        tfPasswordView.setBackground(design.getBackgroundComponents());
-        tfPasswordView.setForeground(design.getTextColor());
-        tfPasswordView.setFont(design.getTextFont());
-        tfPasswordView.setCaretColor(design.getCaretColor());
-        tfPasswordView.setBorder(design.getBorder());
         tfPasswordView.setEditable(false);
 
         pnlPasswordView = new JPanel(new BorderLayout(0, 5));
         pnlPasswordView.add(lblPaswordViewDesc, BorderLayout.NORTH);
         pnlPasswordView.add(tfPasswordView, BorderLayout.CENTER);
-        pnlPasswordView.setBackground(design.getBackgroundColor());
 
         pnlMain.add(pnlPasswordView);
     }
@@ -73,52 +64,75 @@ public class MainWindow extends JFrame {
      * Builds the Password Options Components.
      * */
     private void buildPasswordOptions() {
-
         lblPasswordLengthDesc = new JLabel();
+        lblPasswordLengthDesc.setVerticalAlignment(SwingConstants.BOTTOM);
+        updatePasswordLengthDesc(20);
+        sPasswordLength = new JSlider(1, 200, 20);
+
+        lblSpecialCharsDesc = new JLabel("Special Character");
+        lblSpecialCharsDesc.setVerticalAlignment(SwingConstants.BOTTOM);
+        tfSpecialChars = new JTextField();
+        tfSpecialChars.setText("!\"§$%&/()=?`{[]}\\+*#',;.:-_<>|");
+
+        cmdGenerate = new JButton("Generate");
+
+        pnlPasswordLength = new JPanel(new BorderLayout(0, 5));
+        pnlPasswordLength.add(lblPasswordLengthDesc, BorderLayout.CENTER);
+        pnlPasswordLength.add(sPasswordLength, BorderLayout.SOUTH);
+
+        pnlSpecialCharacter = new JPanel(new BorderLayout(0, 5));
+        pnlSpecialCharacter.add(lblSpecialCharsDesc, BorderLayout.CENTER);
+        pnlSpecialCharacter.add(tfSpecialChars, BorderLayout.SOUTH);
+
+        pnlMain.add(pnlPasswordLength);
+        pnlMain.add(pnlSpecialCharacter);
+        pnlMain.add(cmdGenerate);
+    }
+
+    public void design(WindowDesign design) {
+
+        pnlMain.setBackground(design.getBackgroundColor());
+        pnlMain.setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 40));
+
+        //Password View Component Design
+        lblPaswordViewDesc.setFont(design.getTextFont());
+        lblPaswordViewDesc.setForeground(design.getTextColor());
+        lblPaswordViewDesc.setBackground(design.getBackgroundComponents());
+
+        tfPasswordView.setBackground(design.getBackgroundComponents());
+        tfPasswordView.setForeground(design.getTextColor());
+        tfPasswordView.setFont(design.getTextFont());
+        tfPasswordView.setCaretColor(design.getCaretColor());
+        tfPasswordView.setBorder(design.getBorder());
+
+        pnlPasswordView.setBackground(design.getBackgroundColor());
+
+        //Password Options Component Design
         lblPasswordLengthDesc.setFont(design.getTextFont());
         lblPasswordLengthDesc.setForeground(design.getTextColor());
         lblPasswordLengthDesc.setBackground(design.getBackgroundComponents());
-        lblPasswordLengthDesc.setVerticalAlignment(SwingConstants.BOTTOM);
-        updatePasswordLengthDesc(20);
 
-        sPasswordLength = new JSlider(1, 200, 20);
         sPasswordLength.setBackground(design.getBackgroundComponents());
         sPasswordLength.setForeground(design.getTextColor());
         sPasswordLength.setBorder(design.getBorder());
 
-        lblSpecialCharsDesc = new JLabel("Special Character");
         lblSpecialCharsDesc.setFont(design.getTextFont());
         lblSpecialCharsDesc.setForeground(design.getTextColor());
         lblSpecialCharsDesc.setBackground(design.getBackgroundComponents());
-        lblSpecialCharsDesc.setVerticalAlignment(SwingConstants.BOTTOM);
 
-        tfSpecialChars = new JTextField();
         tfSpecialChars.setBackground(design.getBackgroundComponents());
         tfSpecialChars.setForeground(design.getTextColor());
         tfSpecialChars.setFont(design.getTextFont());
         tfSpecialChars.setCaretColor(design.getCaretColor());
         tfSpecialChars.setBorder(design.getBorder());
-        tfSpecialChars.setText("!\"§$%&/()=?`{[]}\\+*#',;.:-_<>|");
 
-        cmdGenerate = new JButton("Generate");
         cmdGenerate.setBackground(design.getBackgroundComponents());
         cmdGenerate.setForeground(design.getTextColor());
         cmdGenerate.setBorder(design.getBorder());
         cmdGenerate.setFont(design.getHeaderFont());
 
-        pnlPasswordLength = new JPanel(new BorderLayout(0, 5));
-        pnlPasswordLength.add(lblPasswordLengthDesc, BorderLayout.CENTER);
-        pnlPasswordLength.add(sPasswordLength, BorderLayout.SOUTH);
         pnlPasswordLength.setBackground(design.getBackgroundColor());
-
-        pnlSpecialCharacter = new JPanel(new BorderLayout(0, 5));
-        pnlSpecialCharacter.add(lblSpecialCharsDesc, BorderLayout.CENTER);
-        pnlSpecialCharacter.add(tfSpecialChars, BorderLayout.SOUTH);
         pnlSpecialCharacter.setBackground(design.getBackgroundColor());
-
-        pnlMain.add(pnlPasswordLength);
-        pnlMain.add(pnlSpecialCharacter);
-        pnlMain.add(cmdGenerate);
     }
 
     public void addPasswordLengthChangeListener(ChangeListener listener) {
